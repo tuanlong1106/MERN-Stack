d9a979961ab8b418d5e0f67a15e43405d5d6226b
Andrew Nguyen <155884683+tuanlong1106@users.noreply.github.com>
2024-09-16 23:58:16 +0700

    MERN-Stack
    Update MERN-Stack - Beginner's Course

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,50 @@
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
+import { Box, HStack, IconButton, Image, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/toast';
+import { useProductStore } from '../store/product';
+
+const ProductCard = ({ product }) => {
+    const textColor = useColorModeValue('gray.800', 'gray.200');
+    const bg = useColorModeValue('white', 'gray.800');
+    const {deleteProduct} = useProductStore();
+    const toast = useToast();
+    const handleDeleteProduct = async (pid) => {
+        const { success, message } = await deleteProduct(pid);
+        if (!success){
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+        } else {
+            toast({
+                title: "Success",
+                description: message,
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+    return (
+        <Box shadow='lg' rounded='lg' overflow='hidden' transition='all 0.3s' _hover={{ transform: "translateY(-5px)", shadow: "xl" }} bg={bg}>
+            <Image src={product.image} alt={product.name} h={48} w='full' objectFit='cover' />
+            <Box p={4}>
+                <Heading as='h3' size='md' mb={2}>
+                    {product.name}
+                </Heading>
+                <Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
+                    ${product.price}
+                </Text>
+                <HStack>
+                    <IconButton icon={<EditIcon />} colorScheme='blue' />
+                    <IconButton icon={<DeleteIcon />} onClick={() => {handleDeleteProduct(product._id)}} colorScheme='red' />
+                </HStack>
+            </Box>
+        </Box>
+    )
+};
+
+export default ProductCard;
---
+++ frontend/src/store/product.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+    products: [],
+    setProduct: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.image || !newProduct.price) {
+            return { success: false, message: "Please fill all fields" };
+        }
+        const res = await fetch("/api/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProduct),
+        });
+        const data = await res.json();
+        set((state) => ({ products: [...state.products, data.data] }));
+        return { success: true, message: "Product created successfully" };
+    },
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data });
+    },
+    deleteProduct: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        if (!data.success) return { success: false, message: data.message };
+        set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
+        return {success: true, message: data.message};
+    },
+}));
